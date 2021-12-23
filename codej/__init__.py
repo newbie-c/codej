76c2484abc2782f6c8dc4aaf280a42ca6f1a5de9
AndreyVM <webmaster@auriz.ru>
2021-12-23 11:36:26 +0500

    Show favicon and robots.txt

--- codej/__init__.py
+++ codej/__init__.py
@@ -2,14 +2,20 @@ import os
 
 from starlette.applications import Starlette
 from starlette.config import Config
-from starlette.routing import Route
+from starlette.routing import Mount, Route
+from starlette.staticfiles import StaticFiles
 
-from .main.views import show_index
+from .main.views import show_index, show_robots, show_favicon
 
 base = os.path.dirname(__file__)
+static = os.path.join(base, 'static')
 settings = Config(os.path.join(os.path.dirname(base), '.env'))
 
 app = Starlette(
     debug=settings.get('DEBUG', cast=bool),
-    routes=[Route('/', show_index, name='index')])
+    routes=[Route('/', show_index, name='index'),
+            Route('/robots.txt', show_robots, name='robots'),
+            Route('/favicon.ico', show_favicon, name='favicon'),
+            Mount('/static',
+                  app=StaticFiles(directory=static), name='static')])
 app.config = settings
--- codej/main/views.py
+++ codej/main/views.py
@@ -1,16 +1,34 @@
-from starlette.responses import HTMLResponse
+import os
 
-html = """<!DOCTYPE html>
+from starlette.responses import FileResponse, HTMLResponse, PlainTextResponse
+
+html = '''<!DOCTYPE html>
 <html>
   <head>
     <title>CodeJ</title>
+    <link rel="icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
   </head>
   <body>
     <p>Сайт в стадии разработки, попробуйте зайти позже.</p>
   </body>
 </html>
+'''
+robots = """User-agent: *
+Disallow: /
 """
 
 
 async def show_index(request):
    return HTMLResponse(html)
+
+
+async def show_robots(request):
+    if request.method == 'GET':
+        return PlainTextResponse(robots)
+
+
+async def show_favicon(request):
+    if request.method == 'GET':
+        base = os.path.dirname(os.path.dirname(__file__))
+        return FileResponse(
+            os.path.join(base, 'static', 'images', 'favicon.ico'))
