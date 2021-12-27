86d48a155678ed5196de98a67e7c70df2254ad55
AndreyVM <webmaster@auriz.ru>
2021-12-27 14:11:24 +0500

    Add webassets

--- codej/__init__.py
+++ codej/__init__.py
@@ -1,10 +1,15 @@
 import os
 
+import jinja2
+import typing
+
 from starlette.applications import Starlette
 from starlette.config import Config
 from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from webassets import Environment as AssetsEnvironment
+from webassets.ext.jinja2 import assets
 
 from .main.views import show_index, show_robots, show_favicon
 
@@ -12,6 +17,25 @@ base = os.path.dirname(__file__)
 static = os.path.join(base, 'static')
 templates = os.path.join(base, 'templates')
 settings = Config(os.path.join(os.path.dirname(base), '.env'))
+
+
+class J2Templates(Jinja2Templates):
+    def _create_env(self, directory: str) -> "jinja2.Environment":
+        @jinja2.pass_context
+        def url_for(
+                context: dict, name: str, **path_params: typing.Any) -> str:
+            request = context["request"]
+            return request.url_for(name, **path_params)
+
+        loader = jinja2.FileSystemLoader(directory)
+        assets_env = AssetsEnvironment(static, '/static')
+        assets_env.debug = settings.get('ASSETS_DEBUG', bool)
+        env = jinja2.Environment(
+            loader=loader, autoescape=True, extensions=[assets])
+        env.assets_environment = assets_env
+        env.globals["url_for"] = url_for
+        return env
+
 
 app = Starlette(
     debug=settings.get('DEBUG', cast=bool),
@@ -21,4 +45,4 @@ app = Starlette(
             Mount('/static',
                   app=StaticFiles(directory=static), name='static')])
 app.config = settings
-app.jinja = Jinja2Templates(directory=templates)
+app.jinja = J2Templates(directory=templates)
---
+++ dep/README.md
@@ -0,0 +1,5 @@
+Restore vendor's static files:
+
+    cd ~/workspace/codej
+    mkdir -p codej/static/vendor
+    tar -x -z -f dep/static-vendor.tar.gz -C codej/static/vendor
