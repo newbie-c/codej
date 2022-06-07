d591017165dfed17876947f2d3f648c6d8be0c95
AndreyVM <webmaster@codej.ru>
2022-06-07 15:04:20 +0500

    Fix

--- codej/public/views.py
+++ codej/public/views.py
@@ -11,17 +11,15 @@ from .pg import check_topic
 async def show_topic(request):
     slug = request.path_params.get('slug')
     current_user = await checkcu(request)
-    print(current_user)
     if current_user:
         return RedirectResponse(
             request.url_for('arts:show-art', slug=slug), 302)
     conn = await get_conn(request.app.config)
     target = await check_topic(request, conn, slug)
+    await conn.close()
     if target is None:
-        await conn.close()
         raise HTTPException(
             status_code=404, detail='Такой страницы у нас нет.')
-    await conn.close()
     return request.app.jinja.TemplateResponse(
         'public/show-topic.html',
         {'request': request,
