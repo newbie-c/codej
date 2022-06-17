c4145f5c4476b73ab76d241d1ca0d55adcbd82a6
AndreyVM <webmaster@codej.ru>
2022-06-17 13:21:41 +0500

    Show author's blog

--- codej/arts/views.py
+++ codej/arts/views.py
@@ -7,16 +7,44 @@ from ..common.flashed import get_flashed, set_flashed
 from ..common.pg import get_conn
 from ..common.urls import get_next
 from ..drafts.attri import status
-from .pg import check_art, check_last_arts, select_arts
+from .pg import (
+    check_art, check_last_arts, check_last_auth, select_arts,
+    select_auth)
 
 
 async def show_author(request):
     current_user = await checkcu(request)
+    auth = request.path_params.get('username')
+    if current_user is None:
+        await set_flashed(request, 'Требуется авторизация.')
+        return RedirectResponse(
+            await get_next(request, request.app.url_path_for(
+                'arts:show-auth', username=auth)), 302)
+    conn = await get_conn(request.app.config)
+    auth = await conn.fetchrow(
+        '''SELECT id, username, description, last_published FROM users
+             WHERE username = $1
+               AND last_published IS NOT null
+               AND description IS NOT null''', auth)
+    if auth is None or \
+       not (page := await parse_page(request)) or \
+       not (last := await check_last_auth(
+           conn, auth, current_user, page,
+           request.app.config.get('ARTS_PER_PAGE', cast=int, default=3))):
+        await conn.close()
+        raise HTTPException(
+            status_code=404, detail='Такой страницы у нас нет.')
+    pagination = await select_auth(
+        request, conn, auth, current_user, page,
+        request.app.config.get('ARTS_PER_PAGE', cast=int, default=3), last)
+    print(pagination)
+    await conn.close()
     return request.app.jinja.TemplateResponse(
         'arts/show-author.html',
         {'request': request,
          'current_user': current_user,
-         'pagination': None,
+         'pagination': pagination,
+         'author': auth,
          'status': status,
          'flashed': await get_flashed(request)})
 
@@ -39,6 +67,7 @@ async def show_arts(request):
     pagination = await select_arts(
         request, conn, current_user, page,
         request.app.config.get('ARTS_PER_PAGE', cast=int, default=3), last)
+    await conn.close()
     return request.app.jinja.TemplateResponse(
         'arts/show-arts.html',
         {'request': request,
---
+++ codej/static/js/arts/show-author.js
@@ -0,0 +1,14 @@
+$(function() {
+  checkMC(800);
+  hideHidden();
+  $('.content-block').each(checkNext);
+  let dtime = luxon.DateTime.now();
+  if ($('.today-field').length) renderTF('.today-field', dtime);
+  formatFooter(dtime);
+  $('.date-field').each(function() {formatDateTime($(this)); });
+  $('.slidable .block-header').on('click', showHideBlock);
+  $('.close-top-flashed').on('click', closeTopFlashed);
+  $('.entity-link').on('click', function(event) {
+    event.stopPropagation();
+  });
+});
