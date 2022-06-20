8b001ee9828d0313d425ea16e73384020251f26c
AndreyVM <webmaster@codej.ru>
2022-06-20 16:18:57 +0500

    Collect db

--- sql/codej.sql
+++ sql/codej.sql
@@ -60,3 +60,26 @@ CREATE TABLE friends (
     friend_id integer REFERENCES users(id),
     CONSTRAINT author_friend_uni UNIQUE (author_id, friend_id)
 );
+
+CREATE TABLE articles (
+    id serial PRIMARY KEY,
+    title     varchar(100),
+    slug      varchar(128) UNIQUE,
+    suffix    varchar(13)  UNIQUE,
+    html      text         DEFAULT NULL,
+    summary   varchar(512) DEFAULT NULL,
+    meta      varchar(180) DEFAULT NULL,
+    published timestamp    DEFAULT NULL,
+    edited    timestamp,
+    state     varchar(10),
+    commented boolean      DEFAULT TRUE,
+    viewed    integer      DEFAULT 0,
+    author_id integer REFERENCES users(id)
+);
+
+CREATE TABLE paragraphs (
+    num        integer DEFAULT 0,
+    mdtext     text,
+    article_id integer REFERENCES articles(id),
+    CONSTRAINT article_num_uni UNIQUE (article_id, num)
+);
